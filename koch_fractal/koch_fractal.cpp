db6c7e2593ad8084e3fdf13522ea488b17fd1e18
Jasper Rochus <jasper.rochus@gmail.com>
2026-08-18 09:30:07 +0200

    made a start
    ran into a problem with calculating the part of the line until a certain length

---
+++ koch_fractal/koch_fractal.cpp
@@ -0,0 +1,16 @@
+#include "koch_fractal.h"
+
+#include "../base_structs.h"
+#include <SFML/Graphics.hpp>
+
+#include <vector>
+
+
+std::vector<Line> iterate(const std::vector<Line>& prev_lines, float scaling) {
+    std::vector<Line> next_lines = {};
+
+    for (const Line& line : prev_lines) {
+        Line left_str(
+        )
+    }
+}
