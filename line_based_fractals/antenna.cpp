404919694c411d8e37c943e03fec5bfd5d93ceab
Jasper Rochus <jasper.rochus@gmail.com>
2026-09-01 21:40:25 +0200

    Update antenna.cpp

--- line_based_fractals/antenna.cpp
+++ line_based_fractals/antenna.cpp
@@ -13,7 +13,6 @@ std::vector<Line> antenna::iterate(const std::vector<Line>& prev_lines, float wi
     for (const Line& line : prev_lines) {
         float new_width = line.length * width_factor;
         float new_height = line.length * height_factor;
-        float projection_distance = (line.length - new_width) / 2.f;
 
         float angle_rad = std::atan2(line.end.y - line.start.y, line.end.x - line.start.x);
         float angle_deg = angle_rad * 360.f / (2 * PI);
@@ -21,28 +20,32 @@ std::vector<Line> antenna::iterate(const std::vector<Line>& prev_lines, float wi
         sf::Vector2f middle = line.point_on_line_from_start (line.length / 2);
 
         sf::Vector2f left_point = rotate_point_around_anchorpoint(
-            middle - sf::Vector2f(projection_distance, new_height),
+            middle - sf::Vector2f(new_width / 2.f, new_height),
             middle,
             angle_deg
         );
 
         sf::Vector2f right_point = rotate_point_around_anchorpoint(
-            middle - sf::Vector2f(-1.f * projection_distance, new_height),
+            middle - sf::Vector2f(-1.f * new_width / 2.f, new_height),
             middle,
             angle_deg
         );
 
-        float new_length = std::sqrt(std::pow(projection_distance, 2.f) + std::pow(new_height, 2.f));
+        float new_length = std::sqrt(std::pow(new_width / 2.f, 2.f) + std::pow(new_height, 2.f));
 
         Line left(line.start, middle, line.length / 2.f);
         Line right(middle, line.end, line.length / 2.f);
-        Line left_diag(middle, left_point, new_length);
-        Line right_diag(right_point, middle, new_length);
+        Line left_diag_to(middle, left_point, new_length);
+        Line left_diag_from(left_point, middle, new_length);
+        Line right_diag_to(middle, right_point, new_length);
+        Line right_diag_from(right_point, middle, new_length);
 
         next_lines.push_back(left);
         next_lines.push_back(right);
-        next_lines.push_back(left_diag);
-        next_lines.push_back(right_diag);
+        next_lines.push_back(left_diag_to);
+        next_lines.push_back(left_diag_from);
+        next_lines.push_back(right_diag_to);
+        next_lines.push_back(right_diag_from);
     }
     return next_lines;
 }
