7aac03fb9e9371c1451aae856cad76f4ae4e2f80
Jasper Rochus <jasper.rochus@gmail.com>
2026-09-05 16:19:54 +0200

    updated to make it work

--- line_based_fractals/dragon.cpp
+++ line_based_fractals/dragon.cpp
@@ -7,14 +7,15 @@
 #include <vector>
 
 void dragon::iterate(std::vector<Line>& all_lines, float rotation_angle){
-    Line last_line(*all_lines.end());
+    Line last_line(*(all_lines.end() - 1));
     sf::Vector2f anchor = last_line.start;
+    int vector_size = static_cast<int>(all_lines.size());
 
-    for (int line_index = 0; line_index < all_lines.size(); line_index++) {
-        Line line = all_lines.at(line_index);
+    for (int line_index = 0; line_index < vector_size; line_index++) {
+        Line line = all_lines.at(vector_size - line_index - 1);
         Line new_line(
-            rotate_point_around_anchorpoint(line.end, anchor, rotation_angle),
-            rotate_point_around_anchorpoint(line.end, anchor, rotation_angle),
+            rotate_point_around_anchorpoint(line.end, anchor, -rotation_angle),
+            rotate_point_around_anchorpoint(line.start, anchor, -rotation_angle),
             line.length
         );
 
